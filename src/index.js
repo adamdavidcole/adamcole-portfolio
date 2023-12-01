48d4e56213ec8b7ae0c45da46c707bd3d759ea5b
Adam Cole <a.cole@arts.ac.uk>
2023-12-01 00:04:59 +0000

    redirect

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+window.location.replace("https://cowbell-bamboo-jhzl.squarespace.com/");
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
